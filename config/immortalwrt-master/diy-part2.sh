71b0ad406c46c6d2096c0746e5cd4aacf28b8a28
achunbai <47388272+achunbai@users.noreply.github.com>
2024-12-09 19:49:00 +0800

    edit config

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -25,7 +25,45 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-git clone https://github.com/vernesong/OpenClash.git package/openclash
+# 移除 package/openclash（如果存在）
+if [ -d "package/openclash" ]; then
+    rm -rf package/openclash
+    echo "已删除目录 package/openclash。"
+fi
+
+# 创建临时目录
+mkdir -p tmp
+echo "已创建临时目录 tmp。"
+
+# 下载 master.zip 到 tmp 目录
+wget -O tmp/master.zip https://github.com/vernesong/OpenClash/archive/master.zip
+if [ $? -ne 0 ]; then
+    echo "下载 master.zip 失败。"
+    exit 1
+fi
+echo "已下载 master.zip。"
+
+# 解压 master.zip 到 tmp 目录
+unzip tmp/master.zip -d tmp
+if [ $? -ne 0 ]; then
+    echo "解压 master.zip 失败。"
+    exit 1
+fi
+echo "已解压 master.zip 到 tmp 目录。"
+
+# 拷贝解压后的 luci-app-openclash 内容到 package/openclash
+cp -r tmp/OpenClash-master/luci-app-openclash/* package/openclash/
+if [ $? -ne 0 ]; then
+    echo "拷贝文件到 package/openclash 失败。"
+    exit 1
+fi
+echo "已将 luci-app-openclash 拷贝到 package/openclash。"
+
+# 清理临时文件
+rm -rf tmp
+echo "已清理临时目录。"
+
+echo "luci-app-openclash 已成功更新到 package/openclash。"
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
